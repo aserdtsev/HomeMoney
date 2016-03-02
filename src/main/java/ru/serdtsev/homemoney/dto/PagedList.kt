1f4ab562d5e0ea7885ae6b71a3221a7093e5190f
aserdtsev <andrey.serdtsev@gmail.com>
2016-03-02 21:51:05 +0600

    Task. Kotlin trial: class PagedList

--- src/main/java/ru/serdtsev/homemoney/dto/PagedList.kt
+++ src/main/java/ru/serdtsev/homemoney/dto/PagedList.kt
@@ -1,13 +1,6 @@
 package ru.serdtsev.homemoney.dto
 
-import java.io.Serializable
-
-class PagedList<T>(var data: List<T>, limit: Int, offset: Int, hasNext: Boolean?) {
-  var paging: Paging
-
-  init {
-    this.paging = Paging(limit, offset, hasNext)
-  }
-
-  class Paging(var limit: Int, var offset: Int, var hasNext: Boolean?) : Serializable
+class PagedList<T>(var data: List<T>, limit: Int, offset: Int, hasNext: Boolean) {
+  class Paging(var limit: Int, var offset: Int, var hasNext: Boolean)
+  var paging: Paging = Paging(limit, offset, hasNext)
 }
