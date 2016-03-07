c381393abdac9fe463e07e71388713b8c2838f24
aserdtsev <andrey.serdtsev@gmail.com>
2016-03-07 13:49:08 +0600

    Task. Kotlin trial: class MoneyTrnProcessor

--- src/main/java/ru/serdtsev/homemoney/dao/MoneyTrnProcessor.kt
+++ src/main/java/ru/serdtsev/homemoney/dao/MoneyTrnProcessor.kt
@@ -1,8 +1,6 @@
 package ru.serdtsev.homemoney.dao
 
-import com.google.common.base.Strings
 import org.apache.commons.dbutils.BeanProcessor
-
 import java.sql.ResultSet
 import java.sql.SQLException
 import java.util.*
@@ -13,10 +11,11 @@ class MoneyTrnProcessor : BeanProcessor() {
     val columnName = rs.metaData.getColumnName(index)
     when (columnName) {
       "labels" -> {
-        val result = ArrayList<String>()
-        val labels = Optional.ofNullable<String>(Strings.emptyToNull(rs.getString(index)))
-        labels.ifPresent { s -> Collections.addAll(result, *s.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()) }
-        return result
+        val labels = rs.getString(index)
+        if (labels != null)
+          return labels.split(",".toRegex()).dropLastWhile { it.isEmpty() }
+        else
+          return ArrayList<String>()
       }
       else -> return super.processColumn(rs, index, propType)
     }
