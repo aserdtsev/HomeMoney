86b9d3ab2e2eb7c299babcc734eae0f642b53ad9
aserdtsev <andrey.serdtsev@gmail.com>
2019-02-10 20:42:04 +0700

    Bugfix: остатки валютных счетов не учитываются в общем балансе

--- src/main/sql/views.sql
+++ src/main/sql/views.sql
@@ -17,7 +17,7 @@ CREATE OR REPLACE VIEW public.v_crnt_saldo_by_base_cry AS
     balances b
     LEFT JOIN exchange_rates er
       ON substring(er.id from 1 for 3) = b.currency_code
-         and date = (select max(date) from exchange_rates er1 where substring(er.id from 1 for 3) = b.currency_code)
+         and date = (select max(date) from exchange_rates er1 where substring(er1.id from 1 for 3) = b.currency_code)
   WHERE a.type in ('debit', 'credit', 'reserve', 'asset') and b.id = a.id;
 COMMENT ON VIEW public.v_crnt_saldo_by_base_cry
 IS 'Текущие балансы в базовой валюте';
