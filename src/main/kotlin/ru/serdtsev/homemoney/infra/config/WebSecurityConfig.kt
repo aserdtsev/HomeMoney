8b7f408151dd34ebcf59997362b070fb62b45938
Andrey Serdtsev <andrey.serdtsev@gmail.com>
2023-09-15 10:20:45 +0700

    Fix WebSecurityConfig by IDE hints

--- src/main/kotlin/ru/serdtsev/homemoney/infra/config/WebSecurityConfig.kt
+++ src/main/kotlin/ru/serdtsev/homemoney/infra/config/WebSecurityConfig.kt
@@ -1,15 +1,12 @@
 package ru.serdtsev.homemoney.infra.config
 
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.AuthenticationException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 
 
@@ -18,21 +15,16 @@ import org.springframework.security.web.SecurityFilterChain
 class WebSecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain? {
-        http.csrf().disable()
-            .authorizeHttpRequests().requestMatchers("/api/**").hasRole("USER")
-            .anyRequest().permitAll()
-            .and().httpBasic().authenticationEntryPoint(NoPopupBasicAuthenticationEntryPoint())
-            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/**").hasRole("USER")
+                    .anyRequest().permitAll()
+            }
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
         return http.build()
     }
 
     @Bean
     fun encoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
-
-    class NoPopupBasicAuthenticationEntryPoint : AuthenticationEntryPoint {
-        override fun commence(request: HttpServletRequest, response: HttpServletResponse,
-            authException: AuthenticationException) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.message)
-        }
-    }
 }
